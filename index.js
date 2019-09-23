5eb624e17ec510a8681d2dfaf834d9c6762b76ec
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-23 18:03:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ console.log("gfga")
 console.log(arTest)
 console.log(arTest.join(","))*/
 console.log(greeting)
-console.log("Hello,"+ greetSpecialGuest)
+console.log("Hello," + greetSpecialGuest)
